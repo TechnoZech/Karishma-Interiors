3a6766db52b0103c2ca866872a0f141f7e6bfa2a
unknown <sharmadharmesh1998@gmail.com>
2022-11-24 17:18:23 +0530

    responsive navbar created but navlinks needs to be added in next step

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,57 @@
-import React from "react";
-const Logo = require("../images/Logo.png");
+import React, { useState } from "react";
+import NavbarData from "./NavbarData";
+import "./NavbarStyles.css";
 
-function Navbar(){
-    
-    return(
-        <>
-            <nav className="NavbarItems">
-                <img src={Logo} height="40" width="180" alt="Karishma Interiors Logo"/>
-            </nav>
-        </>
-    );
+const Logo = require("../images/Logo-white.png");
+function Navbar() {
+	
+
+    const[clickCvalue, setclickvalue] = useState(false);
+
+    function changesymbol(){
+        setclickvalue(
+            (prevCvalue)=>{
+                if(prevCvalue){
+                    return prevCvalue = false;
+                }else{
+                    return prevCvalue = true;
+                }
+            }
+        )
+    }
+
+
+	return (
+
+		<>
+			<nav className="NavbarItems">
+				<img
+					className="logo"
+					src={Logo}
+					height="40"
+					width="180"
+					alt="Karishma Interiors Logo"
+				/>
+
+				<div className="menu-icons" onClick={changesymbol}>
+					<i className={clickCvalue ? "fas fa-times" : "fas fa-bars"}></i>
+				</div>
+
+				<ul className={clickCvalue ? "nav-menu active" : "nav-menu"}>
+					{NavbarData.map((item, index) => {
+						return (
+							<li key={index}>
+								<a href={item.url} className={item.cName}>
+									<i class={item.icon}></i>
+									{item.title}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</>
+	);
 }
 
 export default Navbar;
---
+++ src/components/NavbarData.js
@@ -0,0 +1,35 @@
+const NavbarData = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links",
+        icon:"fa-solid fa-house-user"
+    },
+    {
+        title: "Service",
+        url: "#",
+        cName: "nav-links",
+        icon:"fa-solid fa-briefcase"
+    },
+    {
+        title: "About",
+        url: "#",
+        cName: "nav-links",
+        icon:"fa-solid fa-circle-info"
+    },
+    {
+        title: "Contact",
+        url: "#",
+        cName: "nav-links",
+        icon:"fa-solid fa-address-book"
+    },
+    {
+        title: "Call Now",
+        url: "#",
+        cName: "nav-btn",
+        icon:"fa-solid fa-phone-volume"
+    },
+    
+]
+
+export default NavbarData;
