ac9e44438cd43c82c4506575d084ce938e2bdedd
unknown <sharmadharmesh1998@gmail.com>
2022-12-03 21:03:09 +0530

    removed old home 2 design and added responsive swiperJs slider cards

--- src/Pages/Home/HomeTwo.jsx
+++ src/Pages/Home/HomeTwo.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
 import "./HomeTwoStyles.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
 
 import Home_two_data from "./HomeTwoData";
 
@@ -10,7 +20,67 @@ function HomeTwo(){
     return(
         <>
             <section className="home_section_two">
-                <div className="home_two_text_container">
+
+            <div className="home_two_text_container">
+                    <h1>One-stop solution for all Interiors</h1>
+                    <p>Whether it’s a magazine-like dream kitchen or your entire home <br/> enjoy end-to-end solutions from design to installation.</p>
+            </div>
+            <div className="home_two_swiper">
+            <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={"auto"}
+                    loop={true}
+                    coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: false,
+                    
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+
+                    {
+                    Home_two_data.map((item, index) => {
+                            return (
+                                <SwiperSlide key={index}>
+                                    <img src={item.src} alt={item.alt} />
+                                    <h2 className="home_two_card_text">{item.alt}</h2>
+                                </SwiperSlide>
+                                
+                            );
+					    }
+                        )
+                    }
+            </Swiper>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* <div className="home_two_text_container">
                     <h1>One-stop solution for all Interiors</h1>
                     <p>Whether it’s a magazine-like dream kitchen or your entire home <br/> enjoy end-to-end solutions from design to installation.</p>
                 </div>
@@ -30,7 +100,8 @@ function HomeTwo(){
                     }
 
 
-                </div>
+                </div> */}
+                                
             </section>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,36 +5,36 @@ import "./NavbarStyles.css";
 
 const Logo = require("../images/Logo.png");
 function Navbar() {
-	
-// <--------------------- To update Hamburger Menu  ------------------------->
+	// <--------------------- To update Hamburger Menu  ------------------------->
 
-    const[clickCvalue, setclickvalue] = useState(false);
+	const [clickCvalue, setclickvalue] = useState(false);
 
-    function changesymbol(){
-        setclickvalue(
-            (prevCvalue)=>{
-                if(prevCvalue){
-                    return prevCvalue = false;
-                }else{
-                    return prevCvalue = true;
-                }
-            }
-        )
-    }
+	function changesymbol() {
+		setclickvalue((prevCvalue) => {
+			if (prevCvalue) {
+				return (prevCvalue = false);
+			} else {
+				return (prevCvalue = true);
+			}
+		});
+	}
 
 	return (
-
 		<>
 			<nav className="NavbarItems">
-				<NavLink to="/" exact className={({ isActive }) => isActive ? undefined : undefined}>
-                    <img
-                        className="logo"
-                        src={Logo}
-                        height="40"
-                        width="180"
-                        alt="Karishma Interiors Logo"
-                    />
-                </NavLink>
+				<NavLink
+					to="/"
+					exact
+					className={({ isActive }) => (isActive ? undefined : undefined)}
+				>
+					<img
+						className="logo"
+						src={Logo}
+						height="40"
+						width="180"
+						alt="Karishma Interiors Logo"
+					/>
+				</NavLink>
 
 				<div className="menu-icons" onClick={changesymbol}>
 					<i className={clickCvalue ? "fas fa-times" : "fas fa-bars"}></i>
@@ -42,26 +42,21 @@ function Navbar() {
 
 				<ul className={clickCvalue ? "nav-menu active" : "nav-menu"}>
 					{NavbarData.map((item, index) => {
-						return (
-							 
-								item.cName === "nav-btn" ? ( // To remove active class from call-now btn
-									
-									<li key={index}>
-										<NavLink to={item.url} className={() => item.cName}>
-											<i class={item.icon}></i>
-											{item.title}
-										</NavLink> 
-									</li>
-								) : (
-									
-									<li key={index}>
-										<NavLink to={item.url} className={item.cName}>
-											<i class={item.icon}></i>
-											{item.title}
-										</NavLink> 
-									</li>
-								)
-							)
+						return item.cName === "nav-btn" ? ( // To remove active class from call-now btn
+							<li key={index}>
+								<NavLink to={item.url} className={() => item.cName}>
+									<i class={item.icon}></i>
+									{item.title}
+								</NavLink>
+							</li>
+						) : (
+							<li key={index}>
+								<NavLink to={item.url} className={item.cName}>
+									<i class={item.icon}></i>
+									{item.title}
+								</NavLink>
+							</li>
+						);
 					})}
 				</ul>
 			</nav>
