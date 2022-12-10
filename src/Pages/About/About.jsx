d0b34c9b62826bf4206e2e0afd5d56dcd5cea85b
unknown <sharmadharmesh1998@gmail.com>
2022-12-10 23:29:48 +0530

    Added about us page with mobile responsive

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./AboutStyles.css";
+import profile from "../../images/profile.jpg";
 
 
 function About(){
     return(
         <>
-            <h1>This is About page</h1>
+            <section className="about_section">
+                <div className="about_container">
+                    <h1 className="about_heading">
+                        About Us
+                    </h1> <br />
+                    <img src={profile} alt="profile" className="about_profile"/>
+                    <h2>Surendar Jangid</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus est sed maiores nostrum, sapiente dicta iusto sint. Incidunt temporibus ab numquam commodi aperiam ullam, provident iusto qui veniam dolor quod illo! Exercitationem commodi tempora amet, sequi ullam maxime repellat a aperiam accusamus quis! Facere adipisci similique vitae! Expedita, enim iure!lorem50
+                    </p>
+                </div>
+            </section>
         </>
     );
 }
